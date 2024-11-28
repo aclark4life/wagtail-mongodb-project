3eb01c6885b4a6dd64de8beddf56b3f6a534c609
Jeffrey A. Clark <aclark@aclark.net>
2024-11-28 13:59:20 -0500

    Add MongoWagtailAdminConfig

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "wagtail.documents",
     "wagtail.images",
     "wagtail.search",
-    "wagtail.admin",
+    "{{ project_name }}.apps.MongoWagtailAdminConfig",
     "wagtail",
     "modelcluster",
     "{{ project_name }}.apps.MongoTaggitConfig",
